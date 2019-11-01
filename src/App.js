a76ea7a07ea6883a5eb30f52911eb6acf8c94419
Wellington Costa <wellington.costa128@gmail.com>
2019-11-01 13:57:00 -0300

    Implementing Not Found page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Router } from '@reach/router';
+import { Redirect, Router } from '@reach/router';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import Home from './components/home/Home';
+import NotFound from './components/common/notfound/NotFound';
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
       <CssBaseline />
       <Router style={{marginTop: 64}}>
         <Home path="/" />
+        <NotFound path="/404" />
+        <Redirect from="*" noThrow to="/404" />
       </Router>
     </ThemeProvider>
   );
